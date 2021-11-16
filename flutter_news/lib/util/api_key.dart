ca62af13591328490ae40a628d615c6204e87f27
18t10 <18t10@DESKTOP-NRD44P2>
2021-11-16 21:21:05 +0700

    first commit

---
+++ flutter_news/lib/util/api_key.dart
@@ -0,0 +1,1 @@
+const String apiKey = 'aca5e7cbacf2415c93ff3ca6e7c03e59';
---
+++ flutter_news/lib/util/topics.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_news/models/topic.dart';
+
+List<Topic> topics = [
+  Topic('Business', 'https://netviethuman.com/wp-content/uploads/2021/06/business-la-gi-1-e1625031577378.jpg'),
+  Topic('Entertainment',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRQyNGT7EOFXUPTDNFYYwWdyBQ5weD7V9U8w&usqp=CAU'),
+  Topic('General',
+      'https://www.pacvn.vn/files/tong-wan-Qg2m1KYB2V.jpg'),
+];
